c29d4e9324a17fbe49cd299d1669e9affeca4188
Dazha Brown <dajahbrown@hotmail.com>
2020-05-11 17:07:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,9 +8,6 @@ def unsafe? (speed)
 end
 
 def not_safe?(speed)
-	puts speed > 60 ? "not_safe" : "safe"
-	puts speed < 40 ? "not_safe" : "safe"
-	puts speed > 60 || speed < 40 ? true : false
-
+	puts speed > 60 ? true : false || speed < 40 ? true : false
 end
 end
